64f99cf6c870532af1f9c283462c0893c8351f52
nebula <3113631254@qq.com>
2025-04-25 00:35:51 +0800

    init db

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import importlib ,os ,pkgutil,inspect
 from entity import *
 
-package_dir = os.path.dirname(__file__)+ '/entity'
-for _, module_name, _ in pkgutil.iter_modules([package_dir]):
-    module = importlib.import_module(f'entity.{module_name}')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///huixue.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-
 
 
 if __name__ == '__main__':
